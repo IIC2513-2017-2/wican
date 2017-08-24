0b0d3be1a28a381407e8e77baa032906fab267fe
Raúl Montes <raulmt@gmail.com>
2017-08-23 21:52:22 -0300

    Add IIC2513 koa template

---
+++ src/routes/hello.js
@@ -0,0 +1,15 @@
+const KoaRouter = require('koa-router');
+
+const router = new KoaRouter();
+
+router.get('hello', '/', async (ctx) => {
+  await ctx.render('hello/index', {
+    nameUrl: name => ctx.router.url('hello.name', name),
+  });
+});
+
+router.get('hello.name', '/:name', (ctx) => {
+  ctx.body = { message: `Hello ${ctx.params.name}!` };
+});
+
+module.exports = router;
