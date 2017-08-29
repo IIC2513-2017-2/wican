46b1b9251d1e18ef35868b9f43029d84d2c088ff
Raúl Montes <raulmt@gmail.com>
2017-08-29 01:06:09 -0300

    Create initiative basic form with server side validations

--- src/models/initiative.js
+++ src/models/initiative.js
@@ -1,7 +1,19 @@
 module.exports = function defineinitiative(sequelize, DataTypes) {
   const initiative = sequelize.define('initiative', {
-    title: DataTypes.STRING,
-    description: DataTypes.TEXT,
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+    },
     image: DataTypes.STRING,
     active: DataTypes.BOOLEAN,
     hashtag: DataTypes.STRING,
--- src/routes/initiatives.js
+++ src/routes/initiatives.js
@@ -5,7 +5,47 @@ const router = new KoaRouter();
 router.get('ongInitiatives', '/', async (ctx) => {
   const ong = await ctx.orm.ong.findById(ctx.params.ongId);
   const initiatives = await ong.getInitiatives();
-  await ctx.render('initiatives/index', { initiatives, ong });
+  await ctx.render('initiatives/index', {
+    initiatives,
+    ong,
+    buildInitiativePath: initiative =>
+      ctx.router.url('ongInitiative', { ongId: initiative.ongId, id: initiative.id }),
+  });
+});
+
+router.get('ongInitiativesNew', '/new', async (ctx) => {
+  const ong = await ctx.orm.ong.findById(ctx.params.ongId);
+  const initiative = ctx.orm.initiative.build();
+  await ctx.render('initiatives/new', {
+    ong,
+    initiative,
+    createInitiativePath: ctx.router.url('ongInitiativesCreate', ong.id),
+  });
+});
+
+router.post('ongInitiativesCreate', '/', async (ctx) => {
+  const ong = await ctx.orm.ong.findById(ctx.params.ongId);
+  try {
+    const initiative = await ong.createInitiative(ctx.request.body);
+    ctx.redirect(ctx.router.url('ongInitiative', { ongId: initiative.ongId, id: initiative.id }));
+  } catch (validationError) {
+    await ctx.render('initiatives/new', {
+      ong,
+      initiative: ctx.orm.initiative.build(ctx.request.body),
+      errors: validationError.errors,
+      createInitiativePath: ctx.router.url('ongInitiativesCreate', ong.id),
+    });
+  }
+});
+
+router.get('ongInitiative', '/:id', async (ctx) => {
+  const ong = await ctx.orm.ong.findById(ctx.params.ongId);
+  const initiatives = await ong.getInitiatives({
+    where: { id: ctx.params.id },
+    limit: 1,
+  });
+  const initiative = initiatives[0];
+  await ctx.render('initiatives/show', { initiative, ong });
 });
 
 module.exports = router;
