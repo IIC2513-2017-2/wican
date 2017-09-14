24332db8aaf5823f8259889800509f0450538f5a
Raúl Montes <raulmt@gmail.com>
2017-09-14 00:26:47 -0300

    Signup form

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ const KoaRouter = require('koa-router');
 const hello = require('./routes/hello');
 const index = require('./routes/index');
 const ongs = require('./routes/ongs');
+const users = require('./routes/users');
 
 const router = new KoaRouter();
 
@@ -15,5 +16,6 @@ router.use((ctx, next) => {
 router.use('/', index.routes());
 router.use('/hello', hello.routes());
 router.use('/ongs', ongs.routes());
+router.use('/users', users.routes());
 
 module.exports = router;
