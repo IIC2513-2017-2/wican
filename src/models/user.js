4654744bd7cfb7aa71c45333bd48b93b3f5d922d
Raúl Montes <raulmt@gmail.com>
2017-09-13 23:50:34 -0300

    Add user model

---
+++ src/models/user.js
@@ -0,0 +1,36 @@
+module.exports = function defineuser(sequelize, DataTypes) {
+  const user = sequelize.define('user', {
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: 'uniqueEmail',
+      validate: {
+        isEmail: true,
+        notEmpty: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+        len: [6],
+      },
+    },
+  });
+  return user;
+};
