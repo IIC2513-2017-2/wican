821ccae634241db73609223daf0d7dd46e63a8b1
Raúl Montes <raulmt@gmail.com>
2017-09-16 00:14:29 -0300

    Layout's header

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,12 @@ const ongs = require('./routes/ongs');
 
 const router = new KoaRouter();
 
+// layout - available to all routes
+router.use((ctx, next) => {
+  ctx.state.ongsPath = ctx.router.url('ongs');
+  return next();
+});
+
 router.use('/', index.routes());
 router.use('/hello', hello.routes());
 router.use('/ongs', ongs.routes());
