aaccfc8735bf27808d6a6444e0627bfbe698038f
Raúl Montes <raulmt@gmail.com>
2017-09-28 02:34:41 -0300

    File handling with Google Cloud Storage

--- src/routes/hello.js
+++ src/routes/hello.js
@@ -2,6 +2,7 @@ const KoaRouter = require('koa-router');
 const sendExampleEmail = require('../mailers/example');
 
 const router = new KoaRouter();
+const fileStorage = require('../services/file-storage');
 
 router.get('hello', '/', async (ctx) => {
   await ctx.render('hello/index', {
@@ -11,14 +12,25 @@ router.get('hello', '/', async (ctx) => {
 });
 
 router.post('hello', '/', async (ctx) => {
-  console.log(ctx.request.body);
+  console.log(ctx.request.body.files);
+  const uploads = ctx.request.body.files.uploads;
+  if (Array.isArray(uploads)) {
+    uploads.forEach(f => fileStorage.upload(f));
+  } else {
+    await fileStorage.upload(ctx.request.body.files.uploads);
+  }
   ctx.flashMessage.notice = 'Form successfully processed';
   // this is just to show how to send an e-mail using a mailer helper fn
   // but it will never be executed
   if (Math.random() > 1) {
-    await sendExampleEmail(ctx.request.body);
+    sendExampleEmail(ctx.request.body);
   }
   ctx.redirect(router.url('hello'));
+});
+
+router.get('hello.file', '/file', (ctx) => {
+  ctx.body = fileStorage.download(ctx.query.file);
+  ctx.response.type = 'image/png';
 });
 
 router.get('hello.name', '/:name', (ctx) => {
