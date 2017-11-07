17db9e5f7dabee7044ea26367618cc589dc6e8b9
Raúl Montes <raulmt@gmail.com>
2017-11-07 00:13:08 -0300

    feat(auth): JWT API Authentication

--- src/routes/api/ongs.js
+++ src/routes/api/ongs.js
@@ -4,6 +4,8 @@ const router = new KoaRouter();
 
 router.get('ongs', '/', async (ctx) => {
   const ongs = await ctx.orm.ong.findAll();
+  // if we have a current user it will be here
+  // console.log('Current user: ', ctx.state.currentUser);
   ctx.body = ctx.jsonSerializer('ongs', {
     attributes: ['name', 'logo', 'email', 'description', 'webpage'],
     topLevelLinks: {
